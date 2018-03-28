6a9252c9f9227a6e29d4e620eec2d08f6a0305c9
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-28 18:34:22 +0530

    Netlify CMS Configurations

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import PricingPagePreview from './preview-templates/PricingPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('pricing', PricingPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
