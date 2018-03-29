c37efc933f56108f9a895efa32f6e21d115e6578
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-29 11:13:55 +0530

    Progressive Web App and Offline Mode

---
+++ data/config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vaibhav on 29/3/18
+ */
+module.exports = {
+    siteTitle: "MakeSamai | Get Started With Superpowered Marketing", // Site title.
+    siteTitleAlt: "MakeSamai", // Alternative site title for SEO.
+    siteLogo: "/logos/logo-512x512.png", // Logo used for SEO and manifest.
+    siteUrl: "https://www.makesamai.com", // Domain of your website without pathPrefix.
+    pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+    siteDescription: "Get Started With Superpowered Marketing", // Website description used for RSS feeds/meta description tag.
+    siteFBAppID: "", // FB Application ID for using app insights
+    googleTagManagerID: "", // GTM tracking ID.
+    disqusShortname: "https-theleakycauldronblog-com", // Disqus shortname.
+    themeColor: "#3273dc", // Used for setting manifest and progress theme colors.
+    backgroundColor: "#ffffff" // Used for setting manifest background color.
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+const config = require("./data/config");
+
 module.exports = {
     siteMetadata: {
-        title: 'MakeSamai | Get Started With Superpowered Marketing',
-        siteUrl: `https://www.makesamai.com`,
+        title: config.siteTitle,
+        siteUrl: config.siteUrl,
     },
     plugins: [
         `gatsby-plugin-react-next`,
@@ -36,6 +38,46 @@ module.exports = {
             resolve: 'gatsby-plugin-netlify-cms',
             options: {
                 modulePath: `${__dirname}/src/cms/cms.js`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: config.siteTitle,
+                short_name: config.siteTitleAlt,
+                start_url: "/",
+                background_color: config.backgroundColor,
+                theme_color: config.themeColor,
+                display: "standalone",
+                icons: [
+                    {
+                        src: `/logos/logo-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/logos/logo-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-nprogress`,
+            options: {
+                // Setting a color is optional.
+                color: config.themeColor,
+                // Disable the loading spinner.
+                showSpinner: false,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-tagmanager`,
+            options: {
+                id: config.googleTagManagerID,
+                includeInDevelopment: false,
             },
         },
     ],
