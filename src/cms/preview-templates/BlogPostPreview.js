78b68c89f1aed6f99f0009b6d01ca0e54698b965
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-29 16:19:03 +0530

    Netlify CMS Preview Config

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import {BlogPostTemplate} from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
+const BlogPostPreview = ({entry, widgetFor}) => (
+    <BlogPostTemplate
+        content={widgetFor('body')}
+        cover={entr.getIn(['data', 'cover'])}
+        meta_title={entry.getIn(['data', 'meta_title'])}
+        meta_desc={entry.getIn(['data', 'meta_description'])}
+        tags={entry.getIn(['data', 'tags'])}
+        title={entry.getIn(['data', 'title'])}
+    />
 )
 
 BlogPostPreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    widgetFor: PropTypes.func,
 }
 
 export default BlogPostPreview
