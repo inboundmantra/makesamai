5aa82803b90f9c08ae1c37b401a9ff46852f11df
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-03-29 12:14:58 +0530

    Favicon Added

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
+import favicon from '../img/favicon.ico'
 import Navbar from '../components/NavBar'
 import Footer from '../components/Footer'
 import './all.sass'
+import config from '../../data/config';
 
 const TemplateWrapper = ({children}) => (
     <div>
-        <Helmet title="MakeSamai, Get Superpowered Marketing"/>
+        <Helmet>
+            <title>{config.siteTitle}</title>
+            <link rel="shortcut icon" href={favicon}/>
+        </Helmet>
         <Navbar/>
         <div>{children()}</div>
         <Footer/>
